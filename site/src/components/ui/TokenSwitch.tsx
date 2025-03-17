8bcce907cd3f597a3eda1f770547b6721d08fea7
Elmore Shaw <elmore.shaw@furo.io>
2025-03-17 20:40:12 +1100

    feat: added token switch

---
+++ site/src/components/ui/TokenSwitch.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/Button";
+import { ArrowUpDown } from "lucide-react";
+
+export function TokenSwitch() {
+  return (
+    <div className="h-0 relative">
+      <Button
+        variant="outline"
+        size="icon"
+        className="h-[17px] w-[35px] rounded-[3px] bg-[#1B1B1F] border-[#09090B] border-[1.5px] p-0 shadow-sm absolute left-1/2 top-[-1px] -translate-x-1/2 -translate-y-1/2 z-20"
+      >
+        <ArrowUpDown className="h-[10px] w-[10px] text-[#A1A1A1]" />
+      </Button>
+    </div>
+  );
+}
+
+export default TokenSwitch;
