5908aafe6bf3c1e82129a4bed33815fb0a87b9e1
Elmore Shaw <elmore.shaw@furo.io>
2025-03-23 15:37:28 +1100

    chore: updating sizing of token switch

--- site/src/components/ui/TokenSwitch.tsx
+++ site/src/components/ui/TokenSwitch.tsx
@@ -7,9 +7,9 @@ export function TokenSwitch() {
       <Button
         variant="outline"
         size="icon"
-        className="h-[17px] w-[35px] rounded-[3px] bg-[#1B1B1F] border-[#09090B] border-[1.5px] p-0 shadow-sm absolute left-1/2 top-[-1px] -translate-x-1/2 -translate-y-1/2 z-20"
+        className="h-[1.75rem] w-[2.5rem] rounded-[3px] bg-[#1B1B1F] border-[#09090B] border-[1.5px] p-0 shadow-sm absolute left-1/2 top-[-1px] -translate-x-1/2 -translate-y-1/2 z-20"
       >
-        <ArrowUpDown className="h-[10px] w-[10px] text-[#A1A1A1]" />
+        <ArrowUpDown className="h-[0.75rem] w-[0.75rem] text-[#A1A1A1]" />
       </Button>
     </div>
   );
