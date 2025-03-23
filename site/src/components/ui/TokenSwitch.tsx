5af1f038dadc274d35e146b20c4f144f4e98710a
Elmore Shaw <elmore.shaw@furo.io>
2025-03-24 07:13:07 +1100

    chore: updated to machineboy's draft

--- site/src/components/ui/TokenSwitch.tsx
+++ site/src/components/ui/TokenSwitch.tsx
@@ -7,9 +7,9 @@ export function TokenSwitch() {
       <Button
         variant="outline"
         size="icon"
-        className="h-[1.75rem] w-[2.5rem] rounded-[3px] bg-[#1B1B1F] border-[#09090B] border-[1.5px] p-0 shadow-sm absolute left-1/2 top-[-1px] -translate-x-1/2 -translate-y-1/2 z-20"
+        className="h-6 w-10 rounded bg-[#1B1B1b] border-[#09090B] border-[1.5px] p-0 shadow-sm absolute left-1/2 top-[-1px] -translate-x-1/2 -translate-y-1/2 z-20"
       >
-        <ArrowUpDown className="h-[0.75rem] w-[0.75rem] text-[#A1A1A1]" />
+        <ArrowUpDown className="h-3 w-3 text-[#A1A1A1]" />
       </Button>
     </div>
   );
